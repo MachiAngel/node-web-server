9f6c0721198b83e5487f677ca3bbaeeb56b03e40
Shiang-Yu Huang <sky90456@hotmail.com>
2017-10-25 22:39:13 +0800

    Add project page

--- server.js
+++ server.js
@@ -57,6 +57,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  });
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs',{
     pageTitle:'Home page',
